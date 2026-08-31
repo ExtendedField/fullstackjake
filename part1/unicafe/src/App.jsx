51157ab70d48857cebe79f53ec38a58f38f67ea4
Jake Benson <jake.benson9@gmail.com>
2026-08-30 20:28:44 -0400

    1.10 (already half complete

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,13 +1,13 @@
 import js from "@eslint/js";
 import { useState } from "react";
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-
 const Header = ({ text }) => (
   <div>
     <h1>{text}</h1>
   </div>
 );
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const Statistics = ({ good, neutral, bad }) => {
   if (good == 0 && neutral == 0 && bad == 0) {
@@ -18,16 +18,16 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <div>
-      <Display text="good" value={good} />
-      <Display text="neutral" value={neutral} />
-      <Display text="bad" value={bad} />
-      <Display text="average" value={average()} />
-      <Display text="percent positive" value={pctPositive()} />
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="average" value={average()} />
+      <StatisticLine text="percent positive" value={pctPositive()} />
     </div>
   );
 };
 
-const Display = ({ text, value }) => {
+const StatisticLine = ({ text, value }) => {
   return (
     <div>
       {text} {value}
@@ -36,7 +36,6 @@ const Display = ({ text, value }) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
